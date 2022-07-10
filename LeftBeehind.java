762dd4a8b078c6f95d7c0304806cebe9638b6e3c
Aditya Prakash Bhagavatula <90626914+AdityaPrakash123@users.noreply.github.com>
2022-07-10 22:10:54 +0300

    Add files via upload

---
+++ LeftBeehind.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class LeftBeehind {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+
+        while (true){
+           int sweet = sc.nextInt();
+           int sour = sc.nextInt();
+
+            if (sweet==0 && sour==0){
+                break;
+            }
+
+           if (sweet+sour == 13){
+               System.out.println("Never speak again.");
+           } else if (sweet>sour) {
+               System.out.println("To the convention.");
+           } else if (sour>sweet) {
+               System.out.println("Left beehind.");
+           } else if (sour==sweet) {
+               System.out.println("Undecided.");
+           }
+
+        }
+
+
+    }
+}
